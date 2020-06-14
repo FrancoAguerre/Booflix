1cec64ddc19d234cbff5c6d416b20959c3fd2f9b
julaq <66279645+julaq@users.noreply.github.com>
2020-06-14 19:05:05 -0300

    Update calc.php

--- critics/calc.php
+++ critics/calc.php
@@ -4,16 +4,20 @@ function calc($bookId){
     include_once "../db.php";
     $conn = conn();
 
+    $newCalif = 3;
     $reviews = mysqli_query($conn,"SELECT * FROM reviews WHERE book_id = $bookId");
-    $reviewsCount = 0;
-    $reviewsValue = 0;
-    while ($reviewRow = mysqli_fetch_assoc($reviews)){
-        if ($reviewRow['calif']>0){
-            $reviewsCount++;
-            $reviewsValue = $reviewsValue + $reviewRow['calif'];
+    if (mysqli_num_rows($reviews)>0){
+        $reviewsCount = 0;
+        $reviewsValue = 0;
+        while ($reviewRow = mysqli_fetch_assoc($reviews)){
+            if ($reviewRow['calif']>0){
+                $reviewsCount++;
+                $reviewsValue = $reviewsValue + $reviewRow['calif'];
+            }
         }
+        $newCalif = round($reviewsValue / $reviewsCount);
     }
-    $newCalif = round($reviewsValue / $reviewsCount);
+    
     mysqli_query($conn,"UPDATE books SET calification = $newCalif WHERE id = '$bookId'");
 }    
 ?>
